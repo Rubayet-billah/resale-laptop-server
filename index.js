60f071f7b2f1c76d13977ba3cc6dbb8a4ce04dc9
Rubayet-billah <rubayetbillah909@gmail.com>
2022-11-24 02:49:37 +0600

    mongodb conneted

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,22 @@ const port = process.env.PORT || 5000;
 // middlewares 
 app.use(cors());
 app.use(express.json());
+
+// database connection
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bhwsqpg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+run().catch(err => console.error(err))
+
 
 app.get('/', (req, res) => {
     res.send('assignment 12 server is running');
