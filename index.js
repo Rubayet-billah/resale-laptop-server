6965b20deed517e1e9b0864b5d2bf22df6f55198
Rubayet-billah <rubayetbillah909@gmail.com>
2022-11-25 04:44:20 +0600

    category route added

--- index.js
+++ index.js
@@ -14,11 +14,40 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bhwsqpg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// database collections
+const usersCollection = client.db('resaleShop').collection('users');
+const categoriesCollection = client.db('resaleShop').collection('categories')
+const productsCollection = client.db('resaleShop').collection('products')
+
 async function run() {
     try {
-
+        // users api
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result)
+        })
+        // categories api
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const categories = await categoriesCollection.find(query).toArray();
+            res.send(categories);
+        })
+        // products api
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const products = await productsCollection.find(query).toArray();
+            res.send(products)
+        })
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { categoryId: id }
+            const product = await productsCollection.find(query).toArray();
+            res.send(product)
+        })
     } catch (error) {
-
+        console.log(error)
     }
 }
 
